e35d407e0c9441e82835032f6c91c5bdee362667
joarndt <joarndt94@gmail.com>
2018-02-22 14:52:41 +0100

    trying stickers

--- src/bot.py
+++ src/bot.py
@@ -83,7 +83,7 @@ class Bot(object):
                     if full_command.__len__() == 2:
                         stickers = self.bot.getStickerSet(full_command[1])
                         print stickers
-                        for x in stickers['stickers']
+                        for x in stickers['stickers']:
                             self.bot.sendSticker(chat_id, x)
                     else:
                         self.bot.sendMessage(chat_id, "only use following syntax: /setStickerSet NAMEOFSET")
