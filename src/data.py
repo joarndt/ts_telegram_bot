db1fb7925054b9d5c6e93c523ddf564f5f449639
joarndt <joarndt94@gmail.com>
2018-03-20 10:33:39 +0100

    hope

--- src/data.py
+++ src/data.py
@@ -83,7 +83,7 @@ class Data(object):
         with open(self.dataPath + 'quotes.pkl', 'wb') as fp:
             return pickle.dump(quotes, fp)
 
-    def addQuote(self, quote, year=datetime.today().year):
+    def addQuote(self, quote, year=int(datetime.today().year)):
         data = self.readQuotes()
         if year in data:
             data[year].append(quote)
