66878a6eb61156f4689936e384e11effaabf8029
joarndt <joarndt94@gmail.com>
2018-04-20 08:25:28 +0200

    fixed birthday wishing

--- src/bot.py
+++ src/bot.py
@@ -305,7 +305,7 @@ class Bot(object):
                 self.bot.getMe()
                 now = datetime.today()
 
-                if now.hour == 8 and now.min == 0:
+                if now.hour == 8 and now.min == 30:
                     birthdays = self.data.readBirthdays()
                     for date in birthdays:
                         for part in birthdays[date]:
