73f621f24a5c9c6e60621b32676824e4a384ba9d
joarndt <joarndt94@gmail.com>
2018-05-21 12:54:32 +0200

    fixing reddit source

--- src/bot.py
+++ src/bot.py
@@ -235,6 +235,7 @@ class Bot(object):
                     message += x.replace(".gifv", ".mp4") + " "
                     send = True
                 elif "redd.it" in x or "reddit.com" in x:
+                    self.bot.sendMessage(self.adminId, "reddit?")
                     text = self.parseUrl(x, 'scrubberThumbSource.*DASH_600_K', 22)
                     if text != "": send = True
                     message += text
