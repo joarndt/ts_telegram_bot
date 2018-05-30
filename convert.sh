e3114970205e47f03d2fbe9a1ad8e0fc10c23813
joarndt <joarndt94@gmail.com>
2018-05-30 17:04:43 +0200

    improved converting script

--- convert.sh
+++ convert.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 filename=$(basename "$1")
-wget "$1"
-mp4_filename="cache/${filename%.*}.mp4"
-ffmpeg -i $filename "$mp4_filename"
-rm $filename
+ffmpeg -i  $1 "cache/${filename%.*}.mp4"
